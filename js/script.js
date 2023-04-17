32e02286bf6e1cb30335c73fe7dbafdc7a65f440
Lucas Lion <lucaslion350@gmail.com>
2023-04-17 09:23:50 -0300

    criando script para fechar menu

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@ $(document).ready(function() {
     var botao = $('.botao');
     var dropDown = $('.ul-sub');    
    
-       botao.on('click', function(event){
-           dropDown.stop(true,true).slideToggle();
-           event.stopPropagation();
-       });
-   });
+    botao.on('click', function(event){
+        dropDown.stop(true,true).slideToggle();
+        event.stopPropagation();
+    });
+
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('.menu').length) {
+            // Fecha o menu
+            $('.ul-sub').slideUp();
+        }
+    });
+});
